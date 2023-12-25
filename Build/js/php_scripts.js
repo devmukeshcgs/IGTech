9e0f6c92577eddc11f8d90547064955a71e1665c
thedotwebstudio <dev.mukeshcgs@gmail.com>
2023-12-25 18:51:27 +0530

    build folder added

---
+++ Build/js/php_scripts.js
@@ -0,0 +1,77 @@
+/* Template: Evolo - StartUp HTML Landing Page Template
+   Author: Inovatik
+   Created: June 2019
+   Description: Custom JS file
+*/
+
+
+(function ($) {
+    "use strict";
+
+    /* Contact Form */
+    $("#contactForm").validator().on("submit", function (event) {
+        if (event.isDefaultPrevented()) {
+            // handle the invalid form...
+            cformError();
+            csubmitMSG(false, "Please fill all fields!");
+        } else {
+            // everything looks good!
+            event.preventDefault();
+            csubmitForm();
+        }
+    });
+
+    function csubmitForm() {
+        // initiate variables with form content
+        var name = $("#cname").val();
+        var email = $("#cemail").val();
+        var message = $("#cmessage").val();
+        var terms = $("#cterms").val();
+        $.ajax({
+            type: "POST",
+            url: "php/contactform-process.php",
+            data: "name=" + name + "&email=" + email + "&message=" + message + "&terms=" + terms,
+            success: function (text) {
+                if (text == "success") {
+                    cformSuccess();
+                } else {
+                    cformError();
+                    csubmitMSG(false, text);
+                }
+            }
+        });
+    }
+
+    function cformSuccess() {
+        $("#contactForm")[0].reset();
+        csubmitMSG(true, "Message Submitted!");
+        $("input").removeClass('notEmpty'); // resets the field label after submission
+        $("textarea").removeClass('notEmpty'); // resets the field label after submission
+        setTimeout(() => {
+            $("#cmsgSubmit").text("");
+           
+        }, 5000);
+
+    }
+
+    function cformError() {
+        $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+            $(this).removeClass();
+        });
+        setTimeout(() => {
+            $("#cmsgSubmit").text("");
+           
+        }, 5000);
+
+    }
+
+    function csubmitMSG(valid, msg) {
+        if (valid) {
+            var msgClasses = "h3 text-center tada animated";
+        } else {
+            var msgClasses = "h3 text-center";
+        }
+        $("#cmsgSubmit").removeClass().addClass(msgClasses).text(msg);
+    }
+
+})(jQuery);
